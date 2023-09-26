861363e133fd3a93a1387e1c136d05f0bb92a14e
Rakshitha Muranga Rodrigo <rakshitha.m.rodrigo@gmail.com>
2023-09-26 16:35:04 -0400

    Initial Commit

---
+++ SwiftUI_Tutorial_Project_/SwiftUI_Tutorial_Project_App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Tutorial_Project_App.swift
+//  SwiftUI_Tutorial_Project_
+//
+//  Created by Rakshitha Rodrigo on 2023-09-26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Tutorial_Project_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

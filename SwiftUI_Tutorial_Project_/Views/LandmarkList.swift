b5189ad74eaf0cf09c371b2bc46e3bbfb840a6bc
Rakshitha Muranga Rodrigo <rakshitha.m.rodrigo@gmail.com>
2023-10-05 11:51:22 -0400

    + Tutorial 3 / Section 3

--- SwiftUI_Tutorial_Project_/Views/LandmarkList.swift
+++ SwiftUI_Tutorial_Project_/Views/LandmarkList.swift
@@ -20,15 +20,20 @@ struct LandmarkList: View {
     
     var body: some View {
         NavigationSplitView {
-            List(filteredLandmarks){ landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
                 }
-                
+                ForEach(filteredLandmarks){ landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                    
+                }
+                .navigationTitle("Landmarks")
             }
-            .navigationTitle("Landmarks")
 //            .scrollIndicators(.hidden)
         } detail: {
             Text("Select a Landmark")

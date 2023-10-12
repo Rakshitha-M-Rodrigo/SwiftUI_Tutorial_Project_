b632d5d2b3108c8722ed1f77c5cc7cc1e6545aed
Rakshitha Muranga Rodrigo <rakshitha.m.rodrigo@gmail.com>
2023-10-11 23:02:58 -0400

    Tutortial 4 - seciton 3-4

---
+++ SwiftUI_Tutorial_Project_/Views/BadgeSymbol.swift
@@ -0,0 +1,18 @@
+//
+//  BadgeSymbol.swift
+//  SwiftUI_Tutorial_Project_LandMarks
+//
+//  Created by Rakshitha Rodrigo on 2023-10-11.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BadgeSymbol()
+}
---
+++ SwiftUI_Tutorial_Project_/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,18 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwiftUI_Tutorial_Project_LandMarks
+//
+//  Created by Rakshitha Rodrigo on 2023-10-11.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol()
+}

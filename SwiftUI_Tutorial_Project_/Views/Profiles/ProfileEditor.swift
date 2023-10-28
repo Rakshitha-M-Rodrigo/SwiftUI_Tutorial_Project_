a849318b329f22390d748ab70923680f81f25044
Rakshitha Muranga Rodrigo <rakshitha.m.rodrigo@gmail.com>
2023-10-28 15:33:16 -0400

    + Tutorial 7 / Section 3

---
+++ SwiftUI_Tutorial_Project_/Views/Profiles/ProfileEditor.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileEditor.swift
+//  SwiftUI_Tutorial_Project_LandMarks
+//
+//  Created by Rakshitha Rodrigo on 2023-10-28.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileEditor()
+}

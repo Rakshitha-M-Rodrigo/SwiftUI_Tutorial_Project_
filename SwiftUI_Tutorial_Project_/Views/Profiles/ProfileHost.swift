eea840c30bb7ad069640af650e9d027b9a612cc0
Rakshitha Muranga Rodrigo <rakshitha.m.rodrigo@gmail.com>
2023-10-28 14:59:26 -0400

    + Tutorial 7 / Section 2

--- SwiftUI_Tutorial_Project_/Views/Profiles/ProfileHost.swift
+++ SwiftUI_Tutorial_Project_/Views/Profiles/ProfileHost.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
+    @Environment(ModelData.self) var modelData
     @State private var draftProfile = Profile.default
     
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-            ProfileSummary(profile: draftProfile)
+            HStack{
+                Spacer()
+                EditButton()
+            }
+            
+            if(editMode?.wrappedValue == .inactive){
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                Text("Profile Button Editor")
+            }
+            
+            
         }
         .padding()
     }
@@ -20,6 +33,7 @@ struct ProfileHost: View {
 
 #Preview {
     ProfileHost()
+        .environment(ModelData())
 }
 
 //struct ProfileHost_Previews: PreviewProvider {

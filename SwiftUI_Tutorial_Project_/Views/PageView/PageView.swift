82f2be1f3bb43c966bced1b5e2efdbab51e3d774
Rakshitha Muranga Rodrigo <rakshitha.m.rodrigo@gmail.com>
2023-10-28 23:44:25 -0400

    + Tutorial 8 / Section 1 - fixes

---
+++ SwiftUI_Tutorial_Project_/Views/PageView/PageView.swift
@@ -0,0 +1,22 @@
+//
+//  PageView.swift
+//  SwiftUI_Tutorial_Project_LandMarks
+//
+//  Created by Rakshitha Rodrigo on 2023-10-28.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    var body: some View {
+        PageViewController(pages: pages)
+    }
+}
+
+#Preview {
+    PageView(pages: ModelData().features.map{
+        FeatureCard(landmark: $0)
+            .aspectRatio(3/2, contentMode: .fit)
+    })
+}

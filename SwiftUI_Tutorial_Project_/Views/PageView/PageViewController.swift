39fa2089e905faea9500dfed57a2f612e8de46c1
Rakshitha Muranga Rodrigo <rakshitha.m.rodrigo@gmail.com>
2023-10-29 00:45:03 -0400

    + Tutorial 8 / Section 2

--- SwiftUI_Tutorial_Project_/Views/PageView/PageViewController.swift
+++ SwiftUI_Tutorial_Project_/Views/PageView/PageViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 import SwiftUI
 
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
     
     var pages: [Page]
     
@@ -17,15 +21,52 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             let pageViewController = UIPageViewController(
                 transitionStyle: .scroll,
                 navigationOrientation: .horizontal)
+        
+        pageViewController.dataSource = context.coordinator
     
             return pageViewController
         }
     
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
-        pageViewController.setViewControllers([UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+//        pageViewController.setViewControllers(
+//            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+        
+        pageViewController.setViewControllers(
+            [context.coordinator.controllers[0]], direction: .forward, animated: true)
     }
     
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if(index == 0){
+                return controllers.last
+            }
+            return controllers[index - 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if(index == controllers.count - 1){
+                return controllers.first
+            }
+            return controllers[index + 1]
+        }
+        
+        var parent: PageViewController
+        var controllers = [UIViewController]()
+        
+        init(_ pageViewController: PageViewController) {
+            parent = pageViewController
+            controllers = parent.pages.map {
+                UIHostingController(rootView: $0)
+            }
+        }
+    }
     
 }
 
